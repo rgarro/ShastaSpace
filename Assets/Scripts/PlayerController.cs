a43e3df79f97b06f94c4a9de813921479b0f695c
rgarro <rgarro@gmail.com>
2017-07-24 05:03:51 -0600

    sunrise pushing vector movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		Rigidbody rigidbody = GetComponent<Rigidbody>();
+		rigidbody.velocity = movement;
 	}
 }
