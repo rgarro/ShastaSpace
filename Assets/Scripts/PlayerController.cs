db9e6fb05d1f8d5e82be970a518d80f5bc92663d
rgarro <rgarro@gmail.com>
2017-07-25 14:06:56 -0600

    r2 db2 the sun the sky they all are blue ...

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-	
+
+	public float speed;
+	public float xMin,xMax,zMin,zMax;
+
 	void FixedUpdate () {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		Rigidbody rigidbody = GetComponent<Rigidbody>();
-		rigidbody.velocity = movement;
+		rigidbody.velocity = movement * speed;
+		rigidbody.position = new Vector3 (
+			Mathf.Clamp(rigidbody.position.x,xMin,xMax),
+			0.0f,
+			Mathf.Clamp(rigidbody.position.z,zMin,zMax)
+		);
 	}
 }
