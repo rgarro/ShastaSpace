65d5c22080cd9d004f62532585824eaff06e3511
rgarro <rgarro@gmail.com>
2017-07-28 03:35:41 -0600

    [Delivers #149597361] betterdorge shudgen delta time

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Mover : MonoBehaviour {
+
+	public float speed;
+
+	void Start () {
+		Rigidbody rigidbody = GetComponent<Rigidbody>();
+		rigidbody.velocity = transform.forward * speed;
+	}
+
+}
