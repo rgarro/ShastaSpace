1b915444ebeb1d52784f3e9d7595a3c522eee2f2
rgarro <rgarro@gmail.com>
2017-08-01 00:21:37 -0600

    [Delivers #149788146] angel  in love fool ..

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour {
+
+	public float tumble;
+
+	// Use this for initialization
+	void Start () {
+		Rigidbody rigidbody = GetComponent<Rigidbody>();
+		rigidbody.angularVelocity = Random.insideUnitSphere * tumble;
+	} 
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
