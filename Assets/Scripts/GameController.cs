355900c662661674504745694af9d01041ec664a
rgarro <rgarro@gmail.com>
2017-08-04 01:12:58 -0600

    [Delivers #149788155]curva del puente negro contrabando de la DIS

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,15 +6,25 @@ public class GameController : MonoBehaviour {
 
 	public GameObject hazard;
 	public Vector3 spawnValues;
-
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
 
 	void Start(){
-		SpawnWaves ();
+		StartCoroutine(SpawnWaves ());
 	}
 
-	void SpawnWaves(){
-		Vector3 spawnPosition = new Vector3 (Random.Range(-spawnValues.x,spawnValues.x),spawnValues.y,spawnValues.z);
-		Quaternion spawnRotation = Quaternion.identity;
-		Instantiate (hazard,spawnPosition,spawnRotation);
+	IEnumerator SpawnWaves(){
+		yield return new WaitForSeconds (startWait);
+		while(true){
+			for (int i = 0; i < hazardCount; i++) {
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);
+		}
 	}
 }
