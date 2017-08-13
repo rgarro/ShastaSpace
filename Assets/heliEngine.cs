d7c456bdef99a669c904dc88bbdec7917b8c8cef
rgarro <rgarro@gmail.com>
2017-08-13 15:06:37 -0600

    [Delivers #150156886] thinking and pointing movement ...

---
+++ Assets/heliEngine.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heliEngine : MonoBehaviour {
+
+	public float rotationSteps;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float rotate = transform.rotation.y + rotationSteps;
+		transform.Rotate(transform.rotation.x,rotate,transform.rotation.x);
+	}
+}
