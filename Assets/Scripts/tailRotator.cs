6a0164b42c7c4a61f1bb00bf399220fd0583e6f5
rgarro <rgarro@gmail.com>
2017-08-25 13:16:39 -0600

    [#150279216] mi mind is set on overdrive ..

---
+++ Assets/Scripts/tailRotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tailRotator : MonoBehaviour {
+
+	public float rotationSteps;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float rotate = transform.rotation.x + rotationSteps;
+		transform.Rotate(rotate,transform.rotation.y,transform.rotation.z);
+	}
+}
